a5e3eeab87769e2a60c751d4a4cc4ffd6c92d310
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-01-04 12:40:35 +0100

    Add project files.

---
+++ MoviesRazor/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MoviesRazor.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Genre> Genres{ get; set; }
+    public virtual DbSet<Movie> Movies { get; set; }
+
+}
---
+++ MoviesRazor/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MoviesRazor.Data;
+
+namespace MoviesRazor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public class MovieItem
+        {
+            public string Title { get; set; }
+            public string Director { get; set; }
+            public int Year { get; set; }
+            public string GenreName { get; set; }
+
+        }
+
+        public List<MovieItem> Items { get; set; }
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+            Items = _context.Movies.Select(m => new MovieItem
+            {
+                Director = m.Director,
+                Title = m.Title,
+                Year = m.Year,
+                GenreName = m.Genre.Name,
+            }).ToList();
+        }
+    }
+}
